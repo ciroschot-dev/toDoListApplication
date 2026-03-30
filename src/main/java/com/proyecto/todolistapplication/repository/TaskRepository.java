84a6d35597e097d3fad9473b71ab5b70026ebf9a
Ciro Schot <cirschot@gmail.com>
2026-03-30 19:52:27 -0300

    Add @Repository annotation to TaskRepository for Spring component scanning

--- src/main/java/com/proyecto/todolistapplication/repository/TaskRepository.java
+++ src/main/java/com/proyecto/todolistapplication/repository/TaskRepository.java
@@ -2,6 +2,9 @@ package com.proyecto.todolistapplication.repository;
 
 import com.proyecto.todolistapplication.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface TaskRepository extends JpaRepository<Task, Long>
 {
