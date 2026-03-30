a5f135dfa900b5caea1c75b1afa789b3e7d7c411
Ciro Schot <cirschot@gmail.com>
2026-03-30 19:21:04 -0300

    Add methods for task management in TaskService interface

--- src/main/java/com/proyecto/todolistapplication/service/TaskService.java
+++ src/main/java/com/proyecto/todolistapplication/service/TaskService.java
@@ -1,6 +1,18 @@
 package com.proyecto.todolistapplication.service;
 
+import com.proyecto.todolistapplication.model.Task;
+
+import java.util.List;
+
 public interface TaskService
 {
+    Task createTask(Task task);
 
+    List<Task> findAllTasks();
+
+    Task findTaskById(long id);
+
+    Task updateTask(Task task, long id);
+
+    void deleteTask(long id);
 }
