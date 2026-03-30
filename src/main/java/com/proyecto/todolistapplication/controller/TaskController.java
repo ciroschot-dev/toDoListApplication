9140e162e7530a97975113d913b45221f7353cef
Ciro Schot <cirschot@gmail.com>
2026-03-30 17:43:29 -0300

    Add TaskController with REST annotations and TaskService dependency

--- src/main/java/com/proyecto/todolistapplication/controller/TaskController.java
+++ src/main/java/com/proyecto/todolistapplication/controller/TaskController.java
@@ -1,5 +1,19 @@
 package com.proyecto.todolistapplication.controller;
 
+import com.proyecto.todolistapplication.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/todo-app")
+@RestController
+
 public class TaskController
 {
+    private final TaskService taskService;
+
+
+
 }
