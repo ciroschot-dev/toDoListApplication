89dbcaedd2e6dba5ab8fa20410a747b9af45f699
Ciro Schot <cirschot@gmail.com>
2026-03-30 19:31:23 -0300

    Add GlobalExceptionHandler to manage TaskNotFoundException

---
+++ src/main/java/com/proyecto/todolistapplication/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.proyecto.todolistapplication.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class GlobalExceptionHandler
+{
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(TaskNotFoundException exception)
+    {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+}
