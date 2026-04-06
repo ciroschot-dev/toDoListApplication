40a61a1e505999d09bd79388e033f5a227b5c77d
Ciro Schot <cirschot@gmail.com>
2026-04-06 11:28:27 -0300

    New class UserService and add user registration method with encoding for passwords

--- src/main/java/com/proyecto/todolistapplication/service/UserService.java
+++ src/main/java/com/proyecto/todolistapplication/service/UserService.java
@@ -1,33 +1,23 @@
 package com.proyecto.todolistapplication.service;
 
-import com.proyecto.todolistapplication.model.MyUserDetails;
 import com.proyecto.todolistapplication.model.User;
 import com.proyecto.todolistapplication.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.NullMarked;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@NullMarked
 
-public class UserService implements UserDetailsService
+public class UserService
 {
     private final UsersRepository repo;
+    private final PasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    public User registerUser(User user)
     {
-        User user = repo.findByUsername(username);
-
-        if (user == null)
-        {
-            throw new UsernameNotFoundException("User not found with username: " + username);
-        }
-
-        return new MyUserDetails(user);
+        user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
     }
 }
