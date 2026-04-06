5b981ab8d8a20aad1ac3e724e449eb5afdd384fc
Ciro Schot <cirschot@gmail.com>
2026-04-06 11:22:05 -0300

    Changed the name of the class

---
+++ src/main/java/com/proyecto/todolistapplication/service/MyUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.proyecto.todolistapplication.service;
+
+import com.proyecto.todolistapplication.model.MyUserDetails;
+import com.proyecto.todolistapplication.model.User;
+import com.proyecto.todolistapplication.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@NullMarked
+
+public class MyUserDetailsService implements UserDetailsService
+{
+    private final UsersRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        User user = repo.findByUsername(username);
+
+        if (user == null)
+        {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return new MyUserDetails(user);
+    }
+}
