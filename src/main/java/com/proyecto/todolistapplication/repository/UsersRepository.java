e966f8694da4eefe375f4b928074372f9c7aaae8
Ciro Schot <cirschot@gmail.com>
2026-04-06 10:48:43 -0300

    Add UsersRepository interface for user data access

---
+++ src/main/java/com/proyecto/todolistapplication/repository/UsersRepository.java
@@ -0,0 +1,12 @@
+package com.proyecto.todolistapplication.repository;
+
+import com.proyecto.todolistapplication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UsersRepository extends JpaRepository<User, Long>
+{
+    User findByUsername(String username);
+}
