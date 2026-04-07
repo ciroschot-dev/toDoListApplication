f8288139621f062d06bd19cd27c504bc029f636c
Ciro Schot <cirschot@gmail.com>
2026-04-07 15:45:58 -0300

    Allow public access to login endpoint and add AuthenticationManager bean

--- src/main/java/com/proyecto/todolistapplication/config/SecurityConfig.java
+++ src/main/java/com/proyecto/todolistapplication/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.proyecto.todolistapplication.config;
 import com.proyecto.todolistapplication.service.MyUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,7 +37,7 @@ public class SecurityConfig
                 .csrf(customizer -> customizer.disable())
                 // Every request requires an authenticated user, except in the register.
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/todo-app/register").permitAll()
+                        .requestMatchers("/todo-app/register", "/todo-app/login").permitAll()
                         .anyRequest().authenticated())
                 // Uses HTTP Basic authentication (username/password on each request).
                 .httpBasic(Customizer.withDefaults())
@@ -51,5 +53,11 @@ public class SecurityConfig
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(encoder);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception
+    {
+        return config.getAuthenticationManager();
     }
 }
