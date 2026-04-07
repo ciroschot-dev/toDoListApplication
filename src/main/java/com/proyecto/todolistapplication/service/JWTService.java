c19496d8b898891b666c267db9b5b4b7bea5581b
Ciro Schot <cirschot@gmail.com>
2026-04-07 16:29:51 -0300

    Refactor JWTService to use application properties for secret key management

--- src/main/java/com/proyecto/todolistapplication/service/JWTService.java
+++ src/main/java/com/proyecto/todolistapplication/service/JWTService.java
@@ -3,13 +3,10 @@ package com.proyecto.todolistapplication.service;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,21 +15,8 @@ import java.util.Map;
 
 public class JWTService
 {
-    String secretKey;
-
-    public JWTService()
-    {
-        try
-        {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
-            SecretKey sk = keyGenerator.generateKey();
-            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     public String generateToken(String username)
     {
