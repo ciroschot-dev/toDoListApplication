b4094680ccf63a377520dec2e852e230d1763d35
Ciro Schot <cirschot@gmail.com>
2026-04-08 08:39:34 -0300

    Add exception handler for BadCredentialsException in GlobalExceptionHandler

--- src/main/java/com/proyecto/todolistapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proyecto/todolistapplication/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.proyecto.todolistapplication.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +14,13 @@ public class GlobalExceptionHandler
     public ResponseEntity<String> handleUserNotFound(TaskNotFoundException exception)
     {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentials(BadCredentialsException exception)
+    {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(exception.getMessage());
     }
 }
