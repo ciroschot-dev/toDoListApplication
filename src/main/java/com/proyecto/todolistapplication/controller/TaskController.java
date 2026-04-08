b918437343db165ac06cb49e0b11d726b7bbcd11
Ciro Schot <cirschot@gmail.com>
2026-04-08 15:12:14 -0300

    Refactor TaskController to use @AuthenticationPrincipal for user retrieval

--- src/main/java/com/proyecto/todolistapplication/controller/TaskController.java
+++ src/main/java/com/proyecto/todolistapplication/controller/TaskController.java
@@ -2,11 +2,10 @@ package com.proyecto.todolistapplication.controller;
 
 import com.proyecto.todolistapplication.model.MyUserDetails;
 import com.proyecto.todolistapplication.model.Task;
-import com.proyecto.todolistapplication.model.User;
 import com.proyecto.todolistapplication.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,46 +20,33 @@ public class TaskController
     private final TaskService taskService;
 
     @PostMapping("/create-task")
-    public ResponseEntity<Task> createTask(@RequestBody Task task)
+    public ResponseEntity<Task> createTask(@RequestBody Task task, @AuthenticationPrincipal MyUserDetails myUserDetails)
     {
-        return ResponseEntity.ok(taskService.createTask(task, getAuthenticatedUser()));
+        return ResponseEntity.ok(taskService.createTask(task, myUserDetails.getUser()));
     }
 
     @RequestMapping
-    public ResponseEntity<List<Task>> findAllTasks()
+    public ResponseEntity<List<Task>> findAllTasks(@AuthenticationPrincipal MyUserDetails myUserDetails)
     {
-        return ResponseEntity.ok(taskService.findAllTasksByUser(getAuthenticatedUser()));
+        return ResponseEntity.ok(taskService.findAllTasksByUser(myUserDetails.getUser()));
     }
 
     @RequestMapping("/find-task/{id}")
-    public ResponseEntity<Task> findTask(@PathVariable long id)
+    public ResponseEntity<Task> findTask(@PathVariable long id, @AuthenticationPrincipal MyUserDetails myUserDetails)
     {
-        return ResponseEntity.ok(taskService.findTaskById(id, getAuthenticatedUser()));
+        return ResponseEntity.ok(taskService.findTaskById(id, myUserDetails.getUser()));
     }
 
     @PutMapping("/update-task/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable long id, @RequestBody Task task)
+    public ResponseEntity<Task> updateTask(@PathVariable long id, @RequestBody Task task, @AuthenticationPrincipal MyUserDetails myUserDetails)
     {
-        return ResponseEntity.ok(taskService.updateTask(task, id, getAuthenticatedUser()));
+        return ResponseEntity.ok(taskService.updateTask(task, id, myUserDetails.getUser()));
     }
 
     @DeleteMapping("/delete-task/{id}")
-    public ResponseEntity<?> deleteTask(@PathVariable long id)
+    public ResponseEntity<?> deleteTask(@PathVariable long id, @AuthenticationPrincipal MyUserDetails myUserDetails)
     {
-        taskService.deleteTask(id, getAuthenticatedUser());
+        taskService.deleteTask(id, myUserDetails.getUser());
         return ResponseEntity.noContent().build();
-    }
-
-    private User getAuthenticatedUser()
-    {
-        MyUserDetails myUserDetails = (MyUserDetails) SecurityContextHolder.getContext()
-                .getAuthentication().getPrincipal();
-
-        if (myUserDetails == null)
-        {
-            throw new NullPointerException("Authenticated user not found");
-        }
-        
-        return myUserDetails.getUser();
     }
 }
