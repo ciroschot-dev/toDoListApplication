709b0a141ef6fb3630edc59d99e937f909391ce6
Ciro Schot <cirschot@gmail.com>
2026-04-08 12:11:50 -0300

    Fix import statement for AccessDeniedException in GlobalExceptionHandler

--- src/main/java/com/proyecto/todolistapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proyecto/todolistapplication/exception/GlobalExceptionHandler.java
@@ -2,11 +2,11 @@ package com.proyecto.todolistapplication.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 
